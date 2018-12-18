8d976753593a8c70b8b51c9d618ced28f233d920
DESKTOP-8HGCRVJ\Ricardo <ricardosabino12@hotmail.com>
2018-12-18 10:00:30 +0000

    projeto

---
+++ Trabalho_Pratico/Servicos_Web/IService1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Servicos_Web
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        bool GetDatalogin(string name, string password);
+
+        [OperationContract]
+        bool RegistUser(string name, string password, int contribuinte, int numero_cc, string rua, int num_porta, int cp);
+
+        [OperationContract]
+        bool DeleteUser(int numero_cc);
+
+        [OperationContract]
+        bool UpdateUser(string name, string password, int contribuinte, int numero_cc, string rua, int num_porta, int cp);
+    }
+
+   
+}
---
+++ Trabalho_Pratico/Servicos_Web/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Servicos_Web
+{
+    public bool GetDatalogin(string name, string password)
+    {
+        return false;
+    }
+
+
+    public bool RegistUser(string name, string password, int contribuinte, int numero_cc, string rua, int num_porta, int cp)
+    {
+        return false;
+    }
+
+
+    public bool DeleteUser(int numero_cc)
+    {
+        return false;
+    }
+
+
+    public bool UpdateUser(string name, string password, int contribuinte, int numero_cc, string rua, int num_porta, int cp)
+    {
+        return false;
+    }
+
+}
